8f20780820443dc618077c32c7f648d4de8244bd
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-02-20 11:28:47 -0300

    fazendo teste do bando de dados

--- model/musica.py
+++ model/musica.py
@@ -10,3 +10,24 @@ def recuperar_msc():
     conexao.close()
 
     return dic_msc
+
+def salvarmusic(cantor:str, duracao:str, nome:str, url_imagem:str, nome_genero:str) -> bool:
+    """
+    O adicionar música vai adicionar as músicas no seu banco de dados.
+    """
+    try:
+        conexao, cursor = conectar()
+
+        cursor.execute("""INSERT INTO musica (cantor, duracao, nome, url_imagem, nome_genero)
+                    VALUES (%s, %s, %s, %s, %s)""",
+                    [cantor, duracao, nome, url_imagem, nome_genero])
+
+        conexao.commit()
+
+        conexao.close()
+
+        return True
+    except:
+        return False
+
+   
---
+++ teste.py
@@ -0,0 +1,3 @@
+from model.musica import salvarmusic
+
+salvarmusic("Matheus&Kauan", "00:03:21", "A Nossa Praia", "https://akamai.sscdn.co/uploadfile/letras/albuns/d/6/7/6/568111489408508.jpg", "Sertanejo")
